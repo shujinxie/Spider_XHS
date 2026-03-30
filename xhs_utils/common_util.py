f1d93f614f35d067bbba099ff86c3bcb1de08d80
shujinxie <103817967+shujinxie@users.noreply.github.com>
2026-03-30 14:28:22 +0800

    docs: add merge conflict resolution guide for updating latest changes

--- xhs_utils/common_util.py
+++ xhs_utils/common_util.py
@@ -7,6 +7,25 @@ def load_env():
     cookies_str = os.getenv('COOKIES')
     return cookies_str
 
+def load_mysql_config():
+    mysql_host = os.getenv('MYSQL_HOST', '')
+    mysql_user = os.getenv('MYSQL_USER', '')
+    mysql_password = os.getenv('MYSQL_PASSWORD', '')
+    mysql_database = os.getenv('MYSQL_DATABASE', '')
+    mysql_port = int(os.getenv('MYSQL_PORT', '3306'))
+    if not all([mysql_host, mysql_user, mysql_password, mysql_database]):
+        return None
+    if mysql_database == 'MYSQL_DATABASE':
+        logger.warning('检测到 MYSQL_DATABASE 使用了占位符字符串，请改成真实库名，例如 xiaohongshudata')
+    return {
+        'host': mysql_host,
+        'port': mysql_port,
+        'user': mysql_user,
+        'password': mysql_password,
+        'database': mysql_database,
+    }
+
+
 def init():
     media_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../datas/media_datas'))
     excel_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../datas/excel_datas'))
